29b46884189eaa6a42da60e7ddc7e2eeb55fb78e
Kakau <kauani.santos@estudante.ifms.edu.br>
2022-09-19 17:09:10 -0400

    finished tudin

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 })
 
+app.get('/formacao', (req, res) => {
+  res.sendFile(path.join(__dirname, '/formacao.html'));
+})
+
+app.get('/pojetos', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pojetos.html'));
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
